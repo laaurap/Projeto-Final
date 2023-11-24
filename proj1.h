683a4e289c34093591c34e9da9092ab25a45c370
laaurap <lauraparente173@gmail.com>
2023-11-24 18:38:48 -0300

    Ajuste de bibliotecas

--- proj1.h
+++ proj1.h
@@ -1,13 +1,14 @@
 #ifndef PROJETO_BIBLIOTECA_H
 #define PROJETO_BIBLIOTECA_H
 
-char status[3][15] = {"Completo", "Em Andamento", "Não iniciado"};
-
-struct lista{ // DEFINIÇÃO DA STRUCT DA LISTA DE TAREFAS
-        int prioridade; //VARIÁVEL QUE VAI ARMAZENAR A PRIORIDADE DA TAREFA (NÚMERO INTEIRO).
-        char categoria[300]; //  ARRAY DE CARACTERES QUE VAI ARMAZENAR A CATEGORIA DA TAREFA
-        char descricao[100]; // ARRAY DE CARACTERES QUE VAI ARMAZENAR A DESCRIÇÃO DA TAREFA
-        int status;         // Corresponde ao indice d array de status
+struct lista {    // DEFINIÇÃO DA STRUCT DA LISTA DE TAREFAS
+  int prioridade; // VARIÁVEL QUE VAI ARMAZENAR A PRIORIDADE DA TAREFA (NÚMERO
+                  // INTEIRO).
+  char categoria[300]; //  ARRAY DE CARACTERES QUE VAI ARMAZENAR A CATEGORIA DA
+                       //  TAREFA
+  char descricao[100]; // ARRAY DE CARACTERES QUE VAI ARMAZENAR A DESCRIÇÃO DA
+                       // TAREFA
+  int status;          // Corresponde ao indice d array de status
 };
 
 void criaTarefa(struct lista *lt);
@@ -21,6 +22,8 @@ void filtraPrioridade(int tam, struct lista *lista, int prioridade);
 void filtraEstado(int tam, struct lista *lista, int estado_indice);
 void filtraCategoria(int tam, struct lista *lista, char categoria[300]);
 void salvaCategoria(int tam, struct lista *lista, char categoria[300]);
-void filtraCategoriaPrioridade(int tam, struct lista *lista, int prioridade, char categoria[300]);
-void salvaCategoriaPrioeidade(int tam, struct lista *lista, int prioridade, char categoria[300]);
+void filtraCategoriaPrioridade(int tam, struct lista *lista, int prioridade,
+                               char categoria[300]);
+void salvaCategoriaPrioeidade(int tam, struct lista *lista, int prioridade,
+                              char categoria[300]);
 #endif
